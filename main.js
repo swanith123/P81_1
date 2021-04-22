26a86bd91b2bc5b0c26c5366fc2195000899ee69
swanith123 <73668844+swanith123@users.noreply.github.com>
2021-04-22 19:12:39 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+canvas =
+    document.getElementById("mycanvas");
+ctx = canvas.getContext("2d")
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(50,50,40,0,2 * Math.PI)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "orange";
+ctx.lineWidth = 5;
+ctx.arc(100,100,40,0,2 * Math.PI)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 5;
+ctx.arc(150,150,40,0,2 * Math.PI)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(200,200,40,0,2 * Math.PI)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(250,250,40,0,2 * Math.PI)
+ctx.stroke();
